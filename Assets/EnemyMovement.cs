f27df156c6c4c2ba15a75ae1bd8bc3caec2d5034
Peter Mullins <jc310xc@gmail.com>
2022-02-02 01:17:58 -0500

    make smol enemy boi walk on a path of waypoints

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float moveSpeed = 1f;
+
+    private int targetIndex = 1;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (targetIndex >= waypoints.Length)
+            return;
+
+        if((Vector2)transform.position == (Vector2) waypoints[targetIndex].position)
+                targetIndex++;
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[targetIndex].position, Time.deltaTime * moveSpeed);
+    }
+}
