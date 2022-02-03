d4e5fb00423d082e89c49daef70985356779c69c
Peter Mullins <jc310xc@gmail.com>
2022-02-03 15:58:31 -0500

    Remove some unneeded logging

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -18,10 +18,7 @@ public class EnemyMovement : MonoBehaviour
             return;
 
         if ((Vector2)transform.position == (Vector2)waypoints[targetIndex].position)
-        {
-            Debug.Log($"Waypoint {targetIndex} reached. Distance to next point: {Vector2.Distance(transform.position, waypoints[targetIndex + 1].position)}. Time: {Time.time}");
             targetIndex++;
-        }
 
         transform.position = Vector2.MoveTowards(transform.position, waypoints[targetIndex].position, Time.deltaTime * moveSpeed);
     }
